105fad8e2c29bc9ea9ea606a41c22e3164451135
Redis Madani <136177376+RedisMadani@users.noreply.github.com>
2024-06-23 18:38:02 +0100

    Update script-06231.sh

--- script-06231.sh
+++ script-06231.sh
@@ -1,33 +1,9 @@
 #!/usr/bin/env bash
-# To use an OpenStack cloud you need to authenticate against the Identity
-# service named keystone, which returns a **Token** and **Service Catalog**.
-# The catalog contains the endpoints for all services the user/tenant has
-# access to - such as Compute, Image Service, Identity, Object Storage, Block
-# Storage, and Networking (code-named nova, glance, keystone, swift,
-# cinder, and neutron).
-#
-# *NOTE*: Using the 3 *Identity API* does not necessarily mean any other
-# OpenStack API is version 3. For example, your cloud provider may implement
-# Image API v1.1, Block Storage API v2, and Compute API v2.0. OS_AUTH_URL is
-# only for the Identity API served through keystone.
 
-# Clear out any previously sourced OpenStack ENV
-for key in $( set | awk '{FS="="}  /^OS_/ {print $1}' ); do unset $key ; done
+# Source the openrc.sh file
+source openrc.sh
 
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_AUTH_URL=https://keystone.rumble.cloud
-
-# With Keystone you pass the keystone password.
-OS_APPLICATION_CREDENTIAL_ID=1e7cebea32a0411bbb47e3b08e291f5f
-export OS_APPLICATION_CREDENTIAL_ID=$OS_APPLICATION_CREDENTIAL_ID
-OS_APPLICATION_CREDENTIAL_SECRET=YObyM_BY4jPS2V7Eu3iwpq50FH_MbRI2197J_bMaJh79373wP2sjJYPpCcKo41snIMlGMWyWsvsPNNNeBgzIuA
-export OS_APPLICATION_CREDENTIAL_SECRET=$OS_APPLICATION_CREDENTIAL_SECRET
-export OS_INTERFACE=public
-export OS_IDENTITY_API_VERSION=3
-
-# If your configuration has multiple regions, we set that information here.
-# OS_REGION_NAME is optional and only valid in certain environments.
-export OS_REGION_NAME=us-east-1
-
-# If OS_AUTH_URL use private SSL, Please add CACERT file path 
-# export OS_CACERT={crtPath}
+# Create instances
+openstack server create --flavor 58ca36f0-7ffa-42e6-aea1-1d4b0471d18a --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default ansible-host
+openstack server create --flavor be1edaa1-2048-425e-b047-70632e487b20 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default mail-server
+openstack server create --flavor f9f64ab1-1288-4cb9-8785-b930d65296b6 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default web-server
