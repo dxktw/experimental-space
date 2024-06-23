ff6e56dc44d8f6bdbc96493b41e06294f5c5f4fe
Redis Madani <136177376+RedisMadani@users.noreply.github.com>
2024-06-23 18:27:32 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,9 @@
-#!/usr/bin/env bash
-# Clear out any previously sourced OpenStack ENV
-for key in $( set | awk '{FS="="}  /^OS_/ {print $1}' ); do unset $key ; done
+#!/bin/bash
 
-export OS_AUTH_TYPE=v3applicationcredential
-export OS_AUTH_URL=https://keystone.rumble.cloud
+# Source the openrc.sh file
+source openrc.sh
 
-# With Keystone, you pass the keystone password.
-export OS_APPLICATION_CREDENTIAL_ID=1e7cebea32a0411bbb47e3b08e291f5f
-export OS_APPLICATION_CREDENTIAL_SECRET=YObyM_BY4jPS2V7Eu3iwpq50FH_MbRI2197J_bMaJh79373wP2sjJYPpCcKo41snIMlGMWyWsvsPNNNeBgzIuA
-export OS_INTERFACE=public
-export OS_IDENTITY_API_VERSION=3
-
-# If your configuration has multiple regions, we set that information here.
-# OS_REGION_NAME is optional and only valid in certain environments.
-export OS_REGION_NAME=us-east-1
-
-# If OS_AUTH_URL uses private SSL, please add CACERT file path 
-# export OS_CACERT={crtPath}
+# Create instances
+openstack server create --flavor 58ca36f0-7ffa-42e6-aea1-1d4b0471d18a --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default ansible-host
+openstack server create --flavor be1edaa1-2048-425e-b047-70632e487b20 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default mail-server
+openstack server create --flavor f9f64ab1-1288-4cb9-8785-b930d65296b6 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default web-server
