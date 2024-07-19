7bcdc8b7b7b1deafbd38dcb395ceec9b8d8c3e5f
Redis Madani <136177376+RedisMadani@users.noreply.github.com>
2024-07-19 13:08:07 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,10 +22,13 @@ openstack security group rule create --proto tcp --dst-port 22 default
 openstack security group rule create --proto tcp --dst-port 80 default
 openstack security group rule create --proto tcp --dst-port 443 default
 
+# Correct image ID for Ubuntu-22.04
+IMAGE_ID=6bdb68e8-bcfc-4e1d-a714-4041f04b1b5e
+
 # Create instances
-openstack server create --flavor 58ca36f0-7ffa-42e6-aea1-1d4b0471d18a --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default ansible-host
-openstack server create --flavor be1edaa1-2048-425e-b047-70632e487b20 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default mail-server
-openstack server create --flavor f9f64ab1-1288-4cb9-8785-b930d65296b6 --image ubuntu-20.04 --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default web-server
+openstack server create --flavor 58ca36f0-7ffa-42e6-aea1-1d4b0471d18a --image $IMAGE_ID --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default ansible-host
+openstack server create --flavor be1edaa1-2048-425e-b047-70632e487b20 --image $IMAGE_ID --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default mail-server
+openstack server create --flavor f9f64ab1-1288-4cb9-8785-b930d65296b6 --image $IMAGE_ID --nic net-id=ffb9d04d-64f4-43e7-bace-0a5fa5935b95 --security-group default web-server
 
 # Check instance status
 for instance in ansible-host mail-server web-server; do
